cca4087842116e5020c48054342204d301f7dd19
Marcello Joaquim <marcellojoaquim1@hotmail.com>
2024-10-19 21:50:33 -0300

    Implementacao dos dtos e modules de auth

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,21 @@
+import { UserSchema } from './schemas/users.schema';
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: 'User',
+        schema: UserSchema,
+      },
+    ]),
+    AuthModule,
+  ],
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService],
 })
 export class UsersModule {}
